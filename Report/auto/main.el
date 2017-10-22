3291bdc5b41bc55a80e75db5a78f958e5e53e30d
Santiago Hincapie <shpotes@hotmail.com>
2017-10-21 19:10:31 -0500

    starbucks

---
+++ Report/auto/main.el
@@ -0,0 +1,41 @@
+(TeX-add-style-hook
+ "main"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "10pt" "twoside")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("xcolor" "table" "xcdraw") ("algorithm" "Alg.")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art10"
+    "inputenc"
+    "natbib"
+    "graphicx"
+    "booktabs"
+    "amsmath"
+    "amsfonts"
+    "adjustbox"
+    "xcolor"
+    "todonotes"
+    "fancyhdr"
+    "multirow"
+    "algorithm"
+    "algpseudocode"
+    "anysize")
+   (TeX-add-symbols
+    '("ignore" 1))
+   (LaTeX-add-labels
+    "sec_intro"
+    "sec_SoA"
+    "sec_mathmod"
+    "sec_alg"
+    "sec_results"
+    "sec_param"
+    "sec_eval"
+    "sec_comp"
+    "sec_conclusions")
+   (LaTeX-add-bibliographies
+    "bibliografia"))
+ :latex)
+
